a206be23361147f98e9fd38068298686e5809b0f
Hamid <www.abdulhamid3311@gmail.com>
2022-04-21 15:20:39 +0600

    connected with mt smart node server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleAddUsers = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+
+    //post data to server
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        const newUser = [...users, data];
+        setUsers(newUser);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My own data {users.length}</h2>
+      <form onSubmit={handleAddUsers} >
+        <input type="text" name='name' placeholder='enter name' required />
+        <input type="email" name='email' placeholder='enter email' required />
+        <input type="submit" value="Add user" />
+      </form>
+
+
+      {
+        users.map(user => <li key={user.id}>Name:{user.name} Email: {user.email}</li>)
+      }
     </div>
   );
 }
